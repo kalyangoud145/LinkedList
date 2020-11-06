4b80fd7d11d647b8e111b34febd8f36d884a9eaf
kalyangoud145 <kalyangoud145@gmail.com>
2020-11-06 21:02:20 +0530

    [Kalyan] added tc for deleting and size methods

--- LinkedListTest.cs
+++ LinkedListTest.cs
@@ -1,10 +1,13 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using LinkedListProject;
+using System.Net.Sockets;
+
 namespace LinkedListTest
 {
     [TestClass]
     public class LinkedListTest
-    {
+    {   
+        ///TC 7.1
         /// <summary>
         /// Givens the element to search in linked list should return true.
         /// </summary>
@@ -21,6 +24,7 @@ namespace LinkedListTest
             //Assert
             Assert.AreEqual(true, result);
         }
+        ///TC 7.2
         /// <summary>
         /// Givens the element to search in linked list should return false.
         /// </summary>
@@ -37,6 +41,7 @@ namespace LinkedListTest
             //Assert
             Assert.AreEqual(false, result);
         }
+        ///TC 8.1
         /// <summary>
         /// Givens the element to insert element in linked list and check if element is inserted should return true.
         /// </summary>
@@ -54,6 +59,10 @@ namespace LinkedListTest
             //Assert
             Assert.IsTrue(result);
         }
+        ///TC 8.2
+        /// <summary>
+        /// Givens the element to insert element in linked list and check if element is inserted should return true.
+        /// </summary>
         [TestMethod]
         public void Given_ElementToInsertElementInLinkedList_AndCheckIfElementIsInserted_Should_Return_True()
         {
@@ -68,5 +77,46 @@ namespace LinkedListTest
             //Assert
             Assert.IsTrue(result);
         }
+        ///TC 9.1
+        /// <summary>
+        /// Givens the element to delete should give new size after deletion true case
+        /// </summary>
+        [TestMethod]
+        public void Given_ElementToDelete_ShouldGiveNewSizeAfterDeletion()
+        {
+            //Arrange
+            int expected = 3;
+            LinkedList list = new LinkedList();
+            list.Add(70);
+            list.Add(30);
+            list.Add(56);
+            list.Add(40);
+            //Act
+            list.Delete(40);
+            int result = list.Size();
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+        ///TC 9.2
+        /// <summary>
+        /// Givens the element to delete should give new size after deletion false case.
+        /// </summary>
+        [TestMethod]
+        public void Given_ElementToDelete_ShouldGiveNewSizeAfterDeletion_FalseCase()
+        {
+            //Arrange
+            int expected = 4;
+            LinkedList list = new LinkedList();
+            list.Add(70);
+            list.Add(30);
+            list.Add(56);
+            list.Add(40);
+            //Act
+            list.Delete(40);
+            int result = list.Size();
+            //Assert
+            Assert.AreNotEqual(expected, result);
+        }
+
     }
 }
