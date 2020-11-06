6788195d32b7968596f79a4f7d530f38314894ad
kalyangoud145 <kalyangoud145@gmail.com>
2020-11-06 12:35:37 +0530

    [Kalyan] created simple linked list

---
+++ Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    /// <summary>
+    /// Node class 
+    /// </summary>
+    class Node
+    {
+        public  int data;
+        public Node next;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+}
